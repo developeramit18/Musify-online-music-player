c94c9c1d427545eaaa93e98ae8a495d189f000d1
amitchahar9837 <amitchahar9837@gmail.com>
2025-09-16 12:35:50 +0530

    updated redis configuration

--- api/lib/redis.js
+++ api/lib/redis.js
@@ -2,4 +2,7 @@ import Redis from "ioredis"
 import dotenv from 'dotenv';
 
 dotenv.config();
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis({
+	url: process.env.UPSTASH_REDIS_URL,
+	token: process.env.UPSTASH_REDIS_TOKEN
+});
