dcacdcce3ce46aef72b27c89e508de80fe3d5fa2
developeramit18 <developeramit7@gmail.com>
2025-09-16 13:11:24 +0530

    Update redis.js

--- api/lib/redis.js
+++ api/lib/redis.js
@@ -2,7 +2,4 @@ import Redis from "ioredis"
 import dotenv from 'dotenv';
 
 dotenv.config();
-export const redis = new Redis({
-	url: process.env.UPSTASH_REDIS_URL,
-	token: process.env.UPSTASH_REDIS_TOKEN
-});
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
